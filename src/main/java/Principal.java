09d6ea9c17db95c7f003323fe9cd304385c215bf
arroz <arroz@DESKTOP-A9MH3CI>
2020-07-22 15:04:22 -0600

    Resolución Expresión #1 del Ejercicio...

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -17,6 +17,10 @@ public class Principal {
         String patronD = "[0-9]";
 
         // Ejemplo con Tipo de Dato String
+        System.out.println("________________________________________\n");
+        System.out.println("              EJEMPLO #1");
+        System.out.println("________________________________________\n");
+        
         String textoAlfanumerico = "0123aaaa";
         System.out.println("Texto Alfanumerico: " + textoAlfanumerico);
 
@@ -26,18 +30,22 @@ public class Principal {
         String replace2 = textoAlfanumerico.replaceAll(patronB, "X");
         System.out.println("Reemplazo de Letras con X: " + replace2);
 
-        //[0-5][a-c];
-        //String patronComplejo = patronA + patronB;
+        // [0-5][a-c];
+        // String patronComplejo = patronA + patronB;
 
-        //[a-c]*[0-5]*
-        //String patronComplejo = patronA + "*" + patronB + "*";
+        // [a-c]*[0-5]*
+        // String patronComplejo = patronA + "*" + patronB + "*";
 
-        //"[a-z]+"
+        // "[a-z]+"
 
         // + = una coincidencia
         // * = ninguna o muchas
 
         //String patronComplejo = "(" + patronA + patronC + ")+";
+        System.out.println("________________________________________\n");
+        System.out.println("              EJEMPLO #2");
+        System.out.println("________________________________________\n");
+        
         String patronComplejo = "(" + patronC + ")+";
 
         String texto = "hola, aacc este bbcc es mi 55222aaa texto 2663aaaa   blah blah";
@@ -50,6 +58,25 @@ public class Principal {
         // Buscar Ocurrencias
         while (matcher.find()) {
             System.out.println("Encontrado: " + matcher.group());
+        }
+        
+        System.out.println("________________________________________\n");
+        System.out.println("             EXPRESION #1");
+        System.out.println("________________________________________\n");
+        
+        String patronComplejo1 = "[ba]+("+"[a-z]*)+";
+
+        String cadena1 = "350 wenas, holu atol barco basurero mantequilla esta hola es mi 1010 00145";
+        
+        System.out.println("Patron Complejo: " + patronComplejo1);
+        System.out.println(cadena1);
+
+        Pattern pattern1 = Pattern.compile(patronComplejo1);
+        Matcher matcher1 = pattern1.matcher(cadena1);
+     
+        // Buscar Ocurrencias
+        while(matcher1.find()) {
+            System.out.println("Encontrado: " + matcher1.group());
         }
     }
 }
